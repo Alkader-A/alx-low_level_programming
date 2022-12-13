e27bbd7b35da9cca33787e0e094233d0433d3854
Alkader-A <tripinfo040@gmail.com>
2022-12-13 00:23:53 -0800

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#inlcude  <unisted.h>
+/**
+ * main -A program that prints a line to the standrd errot 
+ * Return 0 (success)
+ */
+int main(void)
+{
+	fput(" and the piece of Art is useful\" . dora Korpar, 2051-10-19\n, standrd);
+	return(0);
+}
+
