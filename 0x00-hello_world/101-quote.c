21fa9b1b1536fa0bf0f7d577c6f2d9f4b94795a9
Alkader.A <tripinfo040@gmail.com>
2023-03-17 05:49:47 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
-#inlcude  <unisted.h>
+#include <stdio.h>
+#include <unistd.h>
+
 /**
- * main -A program that prints a line to the standrd errot 
- * Return 0 (success)
- */
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fput(" and the piece of Art is useful\" . dora Korpar, 2051-10-19\n, standrd);
-	return(0);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
 
