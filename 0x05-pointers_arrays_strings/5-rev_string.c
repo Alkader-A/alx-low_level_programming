426eb01e80f04462e263d8dcf129087e38e92f5f
Alkader.A <tripinfo040@gmail.com>
2023-03-27 04:37:16 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,9 +14,10 @@ void rev_string(char *s)
                 len++;                                                                                                                
                                                                                                                                       
         for (index = len - 1; index >= len / 2; index--)                                                                              
-        {                                                                                                                             
-                tmp = s[index];                                                                                                       
-                s[index] = s[len - index - 1];                                                                                        
-                s[len - index - 1] = tmp;                                                                                             
+        {                                                                        
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
+                                                                                      
         }                                                                                                                             
 }
